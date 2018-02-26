61eed96096709ebd58ed4b502f68b17535cd9c5e
CC <cauchy.choi@gmail.com>
2018-02-25 20:19:12 -0800

    My first commit

--- lib/espn_scraper/scores.rb
+++ lib/espn_scraper/scores.rb
@@ -1,6 +1,11 @@
+#!/usr/bin/ruby -w
+
 require 'uri'
 require 'cgi'
 require 'json'
+require 'dbi'
+#require 'nokogiri'
+#require 'open-uri'
 
 module ESPN
   SEASONS = {
@@ -105,6 +110,12 @@ module ESPN
 
     alias_method :get_college_football_scores, :get_ncf_scores
 
+	def get_pac12_games(year, week)
+	  markup = Scores.markup_from_year_week_conf('college-football', year, week, 9)
+	  gameIDs = Scores.get_gameIDs(markup)
+	  Scores.get_stats(gameIDs, week)
+	end
+	
     def get_ncb_scores(date, conference_id)
       markup = Scores.markup_from_date_and_conference('ncb', date, conference_id)
       scores = Scores.home_away_parse(markup, date)
@@ -133,6 +144,14 @@ module ESPN
       # Get Markup
 
       def markup_from_year_and_week(league, year, week, group=nil)
+        if group
+          ESPN.get 'scores', league, "scoreboard/_/group/#{group}/year/#{year}/seasontype/2/week/#{week}"
+        else
+          ESPN.get 'scores', league, "scoreboard/_/year/#{year}/seasontype/2/week/#{week}"
+        end
+      end
+	  
+	  def markup_from_year_week_conf(league, year, week, group)
         if group
           ESPN.get 'scores', league, "scoreboard/_/group/#{group}/year/#{year}/seasontype/2/week/#{week}"
         else
@@ -226,6 +245,127 @@ module ESPN
         scores
       end
 
+	  def get_gameIDs(doc)
+        #scores = []
+        games = []
+		#uids = []
+		uid = []
+		count = 0
+        espn_regex = /window\.espn\.scoreboardData \t= (\{.*?\});/
+        doc.xpath("//script").each do |script_section|
+          if script_section.content =~ espn_regex
+            espn_data = JSON.parse(espn_regex.match(script_section.content)[1])
+            games = espn_data['events']
+            break
+          end
+        end
+        games.each do |game|
+          #uid = {}
+		  uidLong = game['uid']
+		  uidShort = uidLong[-9..-1]
+		  uid[count] = uidShort
+		  #score = { league: 'college-football' }
+          #competition = game['competitions'].first
+          #date = DateTime.parse(competition['startDate'])
+          #date = date.new_offset('-06:00')
+          #score[:game_date] = date.to_date
+		  
+		  
+          # Score must be final
+          #if competition['status']['type']['detail'] =~ /^Final/
+          #  competition['competitors'].each do |competitor|
+          #    if competitor['homeAway'] == 'home'
+          #      score[:home_team] = competitor['team']['id'].downcase
+          #      score[:home_score] = competitor['score'].to_i
+          #      else
+          #      score[:away_team] = competitor['team']['id'].downcase
+          #      score[:away_score] = competitor['score'].to_i
+          #    end
+          #  end
+          #  scores << score
+          #end
+		  #uids << uid
+		  count += 1
+        end
+		
+		uid
+        #scores
+      end
+	  
+	  def get_stats(pages, week)
+		#ESPN.get 'scores', league, "scoreboard/_/group/#{group}/year/#{year}/seasontype/2/week/#{week}"
+		stats = []
+		
+		#REAL CODE
+		#pages.each { |page|
+		#html = ESPN.get 'scores', 'college-football', "boxscore?gameId=#{page}"
+		
+		#DEBUG
+		html = ESPN.get 'scores', 'college-football', "boxscore?gameId=#{pages[0]}"
+		#html = ESPN.get 'scores', 'college-football', "boxscore?gameId=400935316"
+		
+		html.xpath("//tbody/tr").each do |player|
+		  stat = {}
+		  if player['class'] != "highlight"
+		    if player.parent.parent.parent.child.child['class'] == "team-name"
+		    #if player.parent.parent.sibling.child.child['class'] == "table-caption"
+		      stat[:teamName] = player.parent.parent.parent.child.child.content.split[0]
+		    end
+		  
+			player.children.each do |playerStat|
+			   #Offense
+			   if playerStat['class'] == "name"
+			      stat[:playerName] = playerStat.content
+				  stat[:week] = week
+			   elsif playerStat['class'] == "c-att"
+				  #stat[:comp_att] = playerStat.content
+				  stat[:completedPasses] = playerStat.content.partition('/').first
+				  stat[:passAttempts] = playerStat.content.partition('/').last
+			   elsif playerStat['class'] == "yds" && player.parent.parent.parent.child.child.content.split[1] == "Passing"
+			      stat[:passingYards] = playerStat.content
+			   elsif playerStat['class'] == "yds" && player.parent.parent.parent.child.child.content.split[1] == "Rushing"
+			      stat[:rushingYards] = playerStat.content
+			   elsif playerStat['class'] == "yds" && player.parent.parent.parent.child.child.content.split[1] == "Receiving"
+				  stat[:receivingYards] = playerStat.content
+			   elsif playerStat['class'] == "td" && player.parent.parent.parent.child.child.content.split[1] == "Passing"
+				  stat[:passingTDs] = playerStat.content
+			   elsif playerStat['class'] == "td" && player.parent.parent.parent.child.child.content.split[1] == "Rushing"
+			      stat[:rushingTDs] = playerStat.content
+			   elsif playerStat['class'] == "td" && player.parent.parent.parent.child.child.content.split[1] == "Receiving"
+			      stat[:receivingTDs] = playerStat.content
+			   elsif playerStat['class'] == "int"
+				  stat[:passingInterceptions] = playerStat.content
+			   elsif playerStat['class'] == "car"
+				  stat[:rushingAttempts] = playerStat.content
+			   elsif playerStat['class'] == "rec" && player.parent.parent.parent.child.child.content.split[1] == "Receiving"
+				  stat[:receptions] = playerStat.content
+			   elsif playerStat['class'] == "lost" && player.parent.parent.parent.child.child.content.split[1] == "Fumbles"
+				  stat[:fumblesLost] = playerStat.content
+			   
+			   #Defense
+			   #elsif playerStat['class'] == "td" && player.parent.parent.parent.child.child.content.split[1] == "Defense"
+			     
+			   
+			   #Kicking
+			   elsif playerStat['class'] == "fg"
+			      #stat[:fieldgoals] = playerStat.content
+				  stat[:fieldGoalAttempts] = playerStat.content.partition('/').last
+				  stat[:fieldGoalsMade] = playerStat.content.partition('/').first
+			   elsif playerStat['class'] == "xp"
+			      stat[:extraPoints] = playerStat.content
+			   end
+			end
+		  end
+
+		  if stat != {}
+			stats << stat
+		  end
+		end
+		#REAL CODE
+		#}
+		puts stats
+	  end
+	  
       def winner_loser_parse(doc, date)
         doc.css('.mod-scorebox-final').map do |game|
           game_info = { game_date: date }
