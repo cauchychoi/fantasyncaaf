f3f07d67703b4489df5da1dd5bf67f84ec70d2c3
CC <cauchy.choi@gmail.com>
2018-03-04 17:01:21 -0800

    test

--- bin/espn_scraper.rb
+++ bin/espn_scraper.rb
@@ -13,7 +13,13 @@ puts ESPN.get_pac12_games(2017, 12)
 #dbh = DBI.connect("DBI:Mysql:localhost:id3779293_ncaafstats", "id3779293_jcl", "jeffcauchylonny")
 
 #client = Mysql2::Client.new(:host => "localhost", :username => "root")
-#client = Mysql2::Client.new(:host => "databases.000webhost.com", :username => "id3779293_jcl", :password => "jeffcauchylonny", :database => "id3779293_ncaafstats")
+#mysql://b4078336a46f7e:10f5241c@us-cdbr-iron-east-05.cleardb.net/heroku_28ca4c386152c4f?reconnect=true
+#DB_HOST = us-cdbr-iron-east-05.cleardb.net
+#DB_DATABASE = heroku_28ca4c386152c4f
+#DB_USERNAME = b4078336a46f7e
+#DB_PASSWORD = 10f5241c
+
+client = Mysql2::Client.new(:host => "us-cdbr-iron-east-05.cleardb.net", :username => "b4078336a46f7e", :password => "10f5241c", :database => "heroku_28ca4c386152c4f")
 #result = client.query("select * from teamRoster")
 #result.each do |row|
 #	puts row.class
