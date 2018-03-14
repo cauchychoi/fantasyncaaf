ac5b541be5dd24033829f50211ddba29b87a7a14
CC <cauchy.choi@gmail.com>
2018-03-14 03:03:38 -0700

    adding scores and fixing table header parsing

--- espn_scraper_main.rb
+++ espn_scraper_main.rb
@@ -10,6 +10,47 @@ puts ESPN.responding?
 #puts ESPN.get_teams_in('ncf')
 #ESPN.get_ncf_scores(2017, 11)
 weeklyStats = ESPN.get_pac12_games(2017, 12)
+
+def calculateScores(stats)
+	stats.each do |statRow|
+		score = 0
+		statRow.each do |statName, statValue|
+			#if statRow.has_key?(:passAttempts) || statRow.has_key?(:rushingAttempts) || statRow.has_key?(:receptions)
+				if statName.to_s.eql?("passingYards")
+					score += statValue.to_f/25.0
+				elsif statName.to_s.eql?("passingTDs")
+					score += statValue.to_f * 4
+				elsif statName.to_s.eql?("passingInterceptions") || statName.to_s.eql?("fumblesLost")
+					score -= statValue.to_f * 2
+				elsif statName.to_s.eql?("rushingYards") || statName.to_s.eql?("receivingYards")
+					score += statValue.to_f/10.0
+				elsif statName.to_s.eql?("rushingTDs") || statName.to_s.eql?("receivingTDs")
+					score += statValue.to_f * 6
+				elsif statName.to_s.eql?("receptions") # Half PPR
+					score += (statValue.to_f)*0.5
+				elsif statName.to_s.eql?("fumblesLost")
+					score -= statValue.to_f * 2
+				
+			#elsif statRow.has_key?(:fumblesRecovered)
+				elsif statName.to_s.eql?("fumblesRecovered") || statName.to_s.eql?("interceptions")
+					score += statValue.to_f * 2
+				elsif statName.to_s.eql?("sacks")
+					score += statValue.to_f * 1
+				elsif statName.to_s.eql?("TDs")
+					score += statValue.to_f * 6
+				
+			#elsif statRow.has_key?(:fieldGoalAttempts)
+				elsif statName.to_s.eql?("extraPoints")
+					score += statValue.to_f * 1
+				end
+			#end
+		end
+		statRow[:fantasyPoints] = score
+	end
+	stats
+end
+
+weeklyStats = calculateScores(weeklyStats)
 puts weeklyStats
 #dbh = DBI.connect("DBI:Mysql:localhost:id3779293_ncaafstats", "id3779293_jcl", "jeffcauchylonny")
 
@@ -80,18 +121,34 @@ weeklyStats.each do |statRow|
 		if (!insert.to_s.eql?("week"))
 			if (tableName.eql?("defenseStats"))
 				if (!insert.to_s.eql?("teamName"))
-					if index == statNames.size - 1
-						queryString += insert.to_s + "=VALUES(" + insert.to_s + ")"
+					if insert.to_s.eql?("fantasyPoints")
+						if index == statNames.size - 1
+							queryString += insert.to_s + "=" + insert.to_s + "+VALUES(" + insert.to_s + ")"
+						else
+							queryString += insert.to_s + "=" + insert.to_s + "+VALUES(" + insert.to_s + "), "
+						end
 					else
-						queryString += insert.to_s + "=VALUES(" + insert.to_s + "), "
+						if index == statNames.size - 1
+							queryString += insert.to_s + "=VALUES(" + insert.to_s + ")"
+						else
+							queryString += insert.to_s + "=VALUES(" + insert.to_s + "), "
+						end
 					end
 				end
 			else
 				if (!insert.to_s.eql?("playerID"))
-					if index == statNames.size - 1
-						queryString += insert.to_s + "=VALUES(" + insert.to_s + ")"
+					if insert.to_s.eql?("fantasyPoints")
+						if index == statNames.size - 1
+							queryString += insert.to_s + "=" + insert.to_s + "+VALUES(" + insert.to_s + ")"
+						else
+							queryString += insert.to_s + "=" + insert.to_s + "+VALUES(" + insert.to_s + "), "
+						end
 					else
-						queryString += insert.to_s + "=VALUES(" + insert.to_s + "), "
+						if index == statNames.size - 1
+							queryString += insert.to_s + "=VALUES(" + insert.to_s + ")"
+						else
+							queryString += insert.to_s + "=VALUES(" + insert.to_s + "), "
+						end
 					end
 				end
 			end
@@ -101,11 +158,15 @@ weeklyStats.each do |statRow|
 	#puts queryString
 	
 	if (!week.to_s.eql?("") && !playerID.to_s.eql?("") && (tableName.eql?("offenseStats") || tableName.eql?("kickerStats")))
+		client.query("truncate " + tableName)
 		client.query(queryString)
 	elsif (!week.to_s.eql?("") && !teamName.to_s.eql?("") && tableName.eql?("defenseStats"))
+		client.query("truncate " + tableName)
 		client.query(queryString)
 	end
 end
+
+
 
 #result = client.query("select * from offenseStats")
 #result.each do |row|
