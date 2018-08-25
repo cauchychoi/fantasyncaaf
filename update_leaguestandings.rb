5a929e3ea8229ca69bbd8e4e7ce8893d3542fc69
CC <cauchy.choi@gmail.com>
2018-08-24 19:05:47 -0700

    automating currentWeek iteration

--- update_leaguestandings.rb
+++ update_leaguestandings.rb
@@ -2,12 +2,47 @@
 
 require 'mysql2'
 
-currentWeekNum = 1
+currentWeek = 1
+if (Time.now.utc > Time.new(2018,11,20,7,0,0,"+00:00"))
+	currentWeek = 13
+elsif (Time.now.utc > Time.new(2018,11,13,7,0,0,"+00:00")) {
+	currentWeek = 12;
+}
+elsif (Time.now.utc > Time.new(2018,11,6,7,0,0,"+00:00")) {
+	currentWeek = 11;
+}
+elsif (Time.now.utc > Time.new(2018,10,30,7,0,0,"+00:00")) {
+	currentWeek = 10;
+}
+elsif (Time.now.utc > Time.new(2018,10,23,7,0,0,"+00:00")) {
+	currentWeek = 9;
+}
+elsif (Time.now.utc > Time.new(2018,10,16,7,0,0,"+00:00")) {
+	currentWeek = 8;
+}
+elsif (Time.now.utc > Time.new(2018,10,9,7,0,0,"+00:00")) {
+	currentWeek = 7;
+}
+elsif (Time.now.utc > Time.new(2018,10,2,7,0,0,"+00:00")) {
+	currentWeek = 6;
+}
+elsif (Time.now.utc > Time.new(2018,9,25,7,0,0,"+00:00")) {
+	currentWeek = 5;
+}
+elsif (Time.now.utc > Time.new(2018,9,18,7,0,0,"+00:00")) {
+	currentWeek = 4;
+}
+elsif (Time.now.utc > Time.new(2018,9,11,7,0,0,"+00:00")) {
+	currentWeek = 3;
+}
+elsif (Time.now.utc > Time.new(2018,9,4,7,0,0,"+00:00")) {
+	currentWeek = 2;
+end
 
 client = Mysql2::Client.new(:host => "us-cdbr-iron-east-05.cleardb.net", :username => "b4078336a46f7e", :password => "10f5241c", :database => "heroku_28ca4c386152c4f")
 puts "Connection successful"
 
-matchupSchedules = client.query("SELECT * FROM matchupschedule WHERE week="+currentWeekNum.to_s)
+matchupSchedules = client.query("SELECT * FROM matchupschedule WHERE week="+currentWeek.to_s)
 
 matchupSchedules.each do |matchup|
 	homeScore = 0
@@ -15,15 +50,15 @@ matchupSchedules.each do |matchup|
 	homeDivision = ""
 	awayDivision = ""
 	
-	client.query("select sum(B.fantasyPoints) as fantasyPoints from teamRoster as A left join(select playerName name,week, fantasyPoints FROM offenseStats union select teamName name,week, fantasyPoints FROM defenseStats) as B on (A.playerName = B.name and A.week = B.week) where hasPlayed = 1 and A.teamID = "+matchup["homeTeamID"].to_s+" and A.week = "+currentWeekNum.to_s).each do |row|
+	client.query("select sum(B.fantasyPoints) as fantasyPoints from teamRoster as A left join(select playerName name,week, fantasyPoints FROM offenseStats union select teamName name,week, fantasyPoints FROM defenseStats) as B on (A.playerName = B.name and A.week = B.week) where hasPlayed = 1 and A.teamID = "+matchup["homeTeamID"].to_s+" and A.week = "+currentWeek.to_s).each do |row|
 		homeScore = row["fantasyPoints"].to_f
 	end
-	client.query("select sum(B.fantasyPoints) as fantasyPoints from teamRoster as A left join(select playerName name,week, fantasyPoints FROM offenseStats union select teamName name,week, fantasyPoints FROM defenseStats) as B on (A.playerName = B.name and A.week = B.week) where hasPlayed = 1 and A.teamID = "+matchup["awayTeamID"].to_s+" and A.week = "+currentWeekNum.to_s).each do |row|
+	client.query("select sum(B.fantasyPoints) as fantasyPoints from teamRoster as A left join(select playerName name,week, fantasyPoints FROM offenseStats union select teamName name,week, fantasyPoints FROM defenseStats) as B on (A.playerName = B.name and A.week = B.week) where hasPlayed = 1 and A.teamID = "+matchup["awayTeamID"].to_s+" and A.week = "+currentWeek.to_s).each do |row|
 		awayScore = row["fantasyPoints"].to_f
 	end
 	
-	#homeScore = client.query("SELECT pointsScored FROM leaguescores WHERE week="+currentWeekNum+" AND teamID="+matchup["homeTeamID"])
-	#awayScore = client.query("SELECT pointsScored FROM leaguescores WHERE week="+currentWeekNum+" AND teamID="+matchup["awayTeamID"])
+	#homeScore = client.query("SELECT pointsScored FROM leaguescores WHERE week="+currentWeek+" AND teamID="+matchup["homeTeamID"])
+	#awayScore = client.query("SELECT pointsScored FROM leaguescores WHERE week="+currentWeek+" AND teamID="+matchup["awayTeamID"])
 	
 	client.query("SELECT division FROM leaguestandings WHERE teamID="+matchup["homeTeamID"].to_s).each do |row|
 		homeDivision = row["division"]
